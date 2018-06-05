62df5268612de1e0366145175c518a464cff5ca4
d-das <jdawsonweehunt@gmail.com>
2018-06-05 11:12:41 -0500

    basic routing in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,31 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { WriterComponent } from './writer/writer.component';
 import { EditorComponent } from './editor/editor.component';
+import { PoetComponent } from './poet/poet.component';
+import { PerformerComponent } from './performer/performer.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
     WriterComponent,
-    EditorComponent
+    EditorComponent,
+    PoetComponent,
+    PerformerComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
